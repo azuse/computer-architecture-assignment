697a9065dbb9ef5849495f2300c4276c8e852a35
SHun Feng <shunf4@live.cn>
2018-03-26 16:11:05 +0800

    ScriptBack 2018-03-26 16:11:04

---
+++ 4-Divider/4-Divider.srcs/sources_1/new/DIV.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/03/26 16:04:25
+// Design Name: 
+// Module Name: DIV
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DIV(
+    input [31:0] dividend,
+    input [31:0] divisor,
+    input start,
+    input clock,
+    input reset,
+    output [31:0] q,
+    output [31:0] r,
+    output busy
+    );
+endmodule
