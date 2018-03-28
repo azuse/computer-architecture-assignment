bc438372dc99f08f0262258193b8b85b6e510743
Shun Feng <shun1048576@zoho.com>
2018-03-28 15:24:41 +0800

    ScriptBack 2018-03-28 15:24:33

--- gitsync.bat
+++ gitsync.bat
@@ -1,8 +1,9 @@
 @echo off
+
 set dd=%DATE:~0,10%
 set tt=%time:~0,8%
 set hour=%tt:~0,2%
-git pull
-git add .
-git commit -m "ScriptBack %dd:/=-% %tt%"
-git push origin master
+git pull > gitlog.log
+git add . >> gitlog.log
+git commit -m "ScriptBack %dd:/=-% %tt%" >> gitlog.log
+git push origin master >> gitlog.log
